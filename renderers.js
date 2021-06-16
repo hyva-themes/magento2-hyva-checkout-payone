f511f2129b41fa168f895b88108bd816a3e8eab1
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2021-06-16 16:56:38 +0530

    [PAYONE-INVOICE]invoice method implemented

--- renderers.js
+++ renderers.js
@@ -1,17 +1,19 @@
 import Ideal from './src/components/iDEAL';
-import CreditCard from './src/components/creditCard';
 import Sofort from './src/components/sofort';
 import PayPal from './src/components/paypal';
 import GiroPay from './src/components/giropay';
+import Invoice from './src/components/invoice';
 import PayDirekt from './src/components/paydirekt';
+import CreditCard from './src/components/creditCard';
 import AdvancePayment from './src/components/advancePayment';
 
 export default {
-  payone_advance_payment: AdvancePayment,
-  payone_creditcard: CreditCard,
-  payone_obt_giropay: GiroPay,
-  payone_obt_ideal: Ideal,
-  payone_obt_sofortueberweisung: Sofort,
-  payone_paydirekt: PayDirekt,
   payone_paypal: PayPal,
+  payone_obt_ideal: Ideal,
+  payone_invoice: Invoice,
+  payone_obt_giropay: GiroPay,
+  payone_paydirekt: PayDirekt,
+  payone_creditcard: CreditCard,
+  payone_obt_sofortueberweisung: Sofort,
+  payone_advance_payment: AdvancePayment,
 };
---
+++ src/components/invoice/Invoice.jsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useEffect } from 'react';
+import { func, shape } from 'prop-types';
+
+import Card from '../../../../../components/common/Card';
+import Checkbox from '../../../../../components/common/Form/Checkbox';
+import RadioInput from '../../../../../components/common/Form/RadioInput';
+import usePerformPlaceOrder from '../../hooks/usePerformPlaceOrder';
+import usePayOneCheckoutFormContext from '../../hooks/usePayOneCheckoutFormContext';
+import { __ } from '../../../../../i18n';
+import invoiceConfig from './invoiceConfig';
+import { paymentMethodShape } from '../../utility';
+import { PAYMENT_METHOD_FORM } from '../../../../../config';
+
+const {
+  instructions,
+  canShowPaymentText,
+  canShowBoniAgreement,
+} = invoiceConfig;
+
+const boniAgreementField = `${PAYMENT_METHOD_FORM}.payone.invoice.boniAgreement`;
+
+function Invoice({ method, selected, actions }) {
+  const { registerPaymentAction } = usePayOneCheckoutFormContext();
+  const isSelected = method.code === selected.code;
+  const performPlaceOrder = usePerformPlaceOrder(method.code);
+
+  const placeOrderWithInvoice = useCallback(
+    values => performPlaceOrder(values),
+    [performPlaceOrder]
+  );
+
+  useEffect(() => {
+    registerPaymentAction(method.code, placeOrderWithInvoice);
+  }, [method, registerPaymentAction, placeOrderWithInvoice]);
+
+  if (!isSelected) {
+    return (
+      <RadioInput
+        label={method.title}
+        name="paymentMethod"
+        value={method.code}
+        onChange={actions.change}
+        checked={isSelected}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <RadioInput
+          label={method.title}
+          name="paymentMethod"
+          value={method.code}
+          onChange={actions.change}
+          checked={isSelected}
+        />
+        {(instructions || canShowPaymentText || canShowBoniAgreement) && (
+          <div className="mx-4 my-4">
+            <Card bg="white">
+              {instructions && <p className="mt-4">{instructions}</p>}
+              {canShowPaymentText && (
+                <div className="mt-4">
+                  <strong>
+                    <span>{invoiceConfig.paymentHintText}</span>
+                  </strong>
+                </div>
+              )}
+              {canShowBoniAgreement && (
+                <div>
+                  <Checkbox
+                    name={boniAgreementField}
+                    label={__(invoiceConfig.agreementMessage)}
+                  />
+                </div>
+              )}
+            </Card>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Invoice.propTypes = {
+  method: paymentMethodShape.isRequired,
+  selected: paymentMethodShape.isRequired,
+  actions: shape({ change: func }),
+};
+
+export default Invoice;
