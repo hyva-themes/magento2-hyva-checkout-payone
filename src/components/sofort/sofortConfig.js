5b22139eb0faa5fe9fbf102ea91a4293848cb8f0
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2021-08-02 15:01:03 +0530

    [SOFORT][FIX]iban & bic fields not showing

--- src/components/sofort/sofortConfig.js
+++ src/components/sofort/sofortConfig.js
@@ -2,15 +2,15 @@ import _get from 'lodash.get';
 
 import RootElement from '../../../../../utils/rootElement';
 
-const paymentMethodCode = 'obt_sofortueberweisung';
+const paymentMethodCode = 'payone_obt_sofortueberweisung';
 const config = RootElement.getPaymentConfig();
-const payOne = _get(config, 'payment.payone');
+const payOne = _get(config, 'payone');
 
 const sofortConfig = {
-  instructions: _get(config, `instructions.${paymentMethodCode}`, ''),
   paymentHintText: _get(payOne, 'paymentHintText'),
   agreementMessage: _get(payOne, 'agreementMessage', ''),
   requestIbanBic: _get(payOne, 'requestIbanBicSofortUeberweisung'),
+  instructions: _get(config, `instructions.${paymentMethodCode}`, ''),
 };
 
 export default sofortConfig;
